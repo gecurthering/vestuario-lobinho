a91f24d29760d86c7e497b647ffb6cf1bc432ba5
vsolymossy <65721339+vsolymossy@users.noreply.github.com>
2020-05-21 11:40:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,83 +1,38 @@
-const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+/**
+* This is the default playground.
+* You should see a bunny spinning in the right preview pane.
+* Feel free to use this as a starting point for you own playground!
+*/
+
+// Create our application instance
+var app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: 0x2c3e50
+});
 document.body.appendChild(app.view);
 
-// create a texture from an image path
-const texture = PIXI.Texture.from('bunny.png');
+// Load the bunny texture
+app.loader.add('bunny', 'https://pixijs.io/examples/examples/assets/bunny.png')
+    .load(startup);
 
-// Scale mode for pixelation
-texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+function startup()
+{
+    var bunny = new PIXI.Sprite(app.loader.resources.bunny.texture);
 
-for (let i = 0; i < 10; i++) {
-    createBunny(
-        Math.floor(Math.random() * app.screen.width),
-        Math.floor(Math.random() * app.screen.height),
-    );
-}
-
-function createBunny(x, y) {
-    // create our little bunny friend..
-    const bunny = new PIXI.Sprite(texture);
-
-    // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
-    bunny.interactive = true;
-
-    // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
-    bunny.buttonMode = true;
-
-    // center the bunny's anchor point
+    // Center the sprite's anchor point
     bunny.anchor.set(0.5);
 
-    // make it a bit bigger, so it's easier to grab
-    bunny.scale.set(3);
+    // Move the sprite to the center of the screen
+    bunny.x = app.renderer.width / 2;
+    bunny.y = app.renderer.height / 2;
 
-    // setup events for mouse + touch using
-    // the pointer events
-    bunny
-        .on('pointerdown', onDragStart)
-        .on('pointerup', onDragEnd)
-        .on('pointerupoutside', onDragEnd)
-        .on('pointermove', onDragMove);
-
-    // For mouse-only events
-    // .on('mousedown', onDragStart)
-    // .on('mouseup', onDragEnd)
-    // .on('mouseupoutside', onDragEnd)
-    // .on('mousemove', onDragMove);
-
-    // For touch-only events
-    // .on('touchstart', onDragStart)
-    // .on('touchend', onDragEnd)
-    // .on('touchendoutside', onDragEnd)
-    // .on('touchmove', onDragMove);
-
-    // move the sprite to its designated position
-    bunny.x = x;
-    bunny.y = y;
-
-    // add it to the stage
     app.stage.addChild(bunny);
-}
 
-function onDragStart(event) {
-    // store a reference to the data
-    // the reason for this is because of multitouch
-    // we want to track the movement of this particular touch
-    this.data = event.data;
-    this.alpha = 0.5;
-    this.dragging = true;
-}
-
-function onDragEnd() {
-    this.alpha = 1;
-    this.dragging = false;
-    // set the interaction data to null
-    this.data = null;
-}
-
-function onDragMove() {
-    if (this.dragging) {
-        const newPosition = this.data.getLocalPosition(this.parent);
-        this.x = newPosition.x;
-        this.y = newPosition.y;
-    }
+    // Listen for animate update
+    app.ticker.add(function(delta)
+    {
+        // Rotate mr rabbit clockwise
+        bunny.rotation += 0.1 * delta;
+    });
 }
